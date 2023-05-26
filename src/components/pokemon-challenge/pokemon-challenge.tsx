48e39e6bdffb66c06fec988e0f4cf05fa3d3acfe
Andres Arcila <andresarcila2@me.com>
2023-05-26 11:18:12 -0500

    random pokemon

--- src/components/pokemon-challenge/pokemon-challenge.tsx
+++ src/components/pokemon-challenge/pokemon-challenge.tsx
@@ -1,15 +1,12 @@
 import { Resource, component$, useResource$ } from "@builder.io/qwik";
-import { getPokemons } from "~/services/pokemonsService";
+import { getRandomPokemon } from "~/services/pokemonsService";
 
 export default component$(() => {
 
     const pokemonsResource = useResource$(({ track, cleanup }) => {
-
         const controller = new AbortController();
         cleanup(() => controller.abort());
-
-        // Fetch the data and return the promises.
-        return getPokemons();
+        return getRandomPokemon();
     });
 
     return (
@@ -17,7 +14,7 @@ export default component$(() => {
             <Resource
                 value={pokemonsResource}
                 onPending={() => <p>Loading...</p>}
-                onResolved={(data) => <h2>{data[0].name}</h2>}
+                onResolved={(data) => <h2>{data.name}</h2>}
             />
         </div>
     )
--- src/services/pokemonsService.ts
+++ src/services/pokemonsService.ts
@@ -1,5 +1,13 @@
-export async function getPokemons() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0');
+export async function getPokemons(max:number) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${max}&offset=0`);
     const data = await response.json();
     return data.results;
+}
+
+export async function getRandomPokemon() {
+    const maxPokedex = 250;
+    const allPokemons = await getPokemons(maxPokedex);
+    var randomNumber = Math.floor(Math.random() * (maxPokedex+1)); // Multiplicado por 151 porque el rango es exclusivo en el límite superior
+    const randPokemon = allPokemons[randomNumber];
+    return randPokemon;
 }
